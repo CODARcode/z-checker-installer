e61d8ac7498bc7b8cdbc3d3618523e48bef111e7
Sheng Di <disheng222@gmail.com>
2017-05-04 13:03:26 -0500

    add z-checker-update.sh

---
+++ z-checker-update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rootDir=`pwd`
+
+#---------- download Z-checker --------------
+cd Z-checker
+git pull
+make
+make install
+cp ../zc-patches/generateReport.sh ./examples/
+
+#---------- download ZFP and set the configuration -----------
+cd $rootDir
+cd zfp
+git pull
+make
+
+cd -
+cp zfp-patches/zfp-zc.c zfp/utils
+cp zfp-patches/*.sh zfp/utils
+
+make
+
+#---------- download SZ and set the configuration -----------
+cd $rootDir
+cd SZ
+git pull
+make
+make install
+
+cd example
+cp ../../Z-checker/examples/zc.config .
+cp ../../sz-patches/sz-zc-ratedistortion.sh .
+cp ../../sz-patches/testfloat_CompDecomp.sh .
+
+
