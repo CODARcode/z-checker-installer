aef96899ab2b16fe2ed908f8b018b4d54af7c190
Sheng Di <disheng222@gmail.com>
2017-05-16 23:30:37 -0500

    fix the previous commented lines.

--- runZCCase.sh
+++ runZCCase.sh
@@ -41,22 +41,22 @@ fi
 
 cd SZ/${testcase}_fast
 echo ./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
-#./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
+./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
 
 cd $rootDir
 cd SZ/${testcase}_deft
 echo ./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
-#./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
+./sz-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
 
 cd $rootDir
 cd zfp/${testcase}
 echo ./zfp-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
-#./zfp-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
+./zfp-zc-ratedistortion.sh $dataDir $dim1 $dim2 $dim3 $dim4
 
 cd $rootDir
 cd Z-checker/${testcase}
 echo ./analyzeDataProperty.sh $dataDir $dim1 $dim2 $dim3 $dim4
-#./analyzeDataProperty.sh $dataDir $dim1 $dim2 $dim3 $dim4
+./analyzeDataProperty.sh $dataDir $dim1 $dim2 $dim3 $dim4
 
 echo ./generateReport.sh ${testcase}
 ./generateReport.sh ${testcase}
