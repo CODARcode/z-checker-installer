26266680e12d67bc3c55b75bb3778f4895fee83d
Hanqi Guo <hguo@anl.gov>
2017-06-07 09:53:49 -0500

    WIP

--- generateWebReport.sh
+++ generateWebReport.sh
@@ -10,9 +10,12 @@ testcase=$1
 
 rootDir=`pwd`
 
-export PATH=$rootDir/node-v6.11.0-install-bin:$PATH
+# export PATH=$rootDir/gnuplot-5.0.6-install/bin:$rootDir/node-v6.11.0-install/bin:$PATH
+export PATH=$rootDir/node-v6.11.0-install/bin:$PATH
 
-node $rootDir/z-checker-web/generate-report.js $rootDir/Z-checker/$testcase
+cd $rootDir/z-checker-web
+npm update
+node generate_report.js $rootDir/Z-checker/$testcase
 
 echo "Please use your web browser to open this webpage:"
 echo "file://"$rootDir/Z-checker/$testcase/report/web/index.htm
