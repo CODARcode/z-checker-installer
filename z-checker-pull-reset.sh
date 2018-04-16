9cbbcf21c4b7a74b66cdfda01c602a6b727fbb48
Sheng Di <sdi1@anl.gov>
2018-04-16 10:57:46 -0500

    modify z-checker-pull-reset.sh to fix compilation issue of manageCompressor.c

--- z-checker-pull-reset.sh
+++ z-checker-pull-reset.sh
@@ -10,12 +10,6 @@ if [ ! -d Z-checker ]; then
 	exit
 fi
 
-#--------- compile codes in zc-patches-------
-cd $rootDir/zc-patches
-gcc -O3 -o queryVarList queryVarList.c
-gcc -g -O3 -o manageCompressor manageCompressor.c -I../Z-checker/zc-install/include ../Z-checker/zc-install/lib/libzc.a -lm
-mv manageCompressor ..
-
 #---------- download Z-checker --------------
 cd $rootDir
 
@@ -55,6 +49,12 @@ cp ../zc-patches/generateReport.sh ./examples/
 cd examples
 make clean
 make
+
+#--------- compile codes in zc-patches-------
+cd $rootDir/zc-patches
+gcc -O3 -o queryVarList queryVarList.c
+gcc -g -O3 -o manageCompressor manageCompressor.c -I../Z-checker/zc-install/include ../Z-checker/zc-install/lib/libzc.a -lm
+mv manageCompressor ..
 
 #---------- download ZFP and set the configuration -----------
 cd $rootDir
