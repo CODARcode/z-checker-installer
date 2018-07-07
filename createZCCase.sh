28fd40202290dd8758f3c1cf8377338cef9e619d
Sheng Di <sdi1@anl.gov>
2018-07-07 00:02:41 -0500

    revise createZCCase.sh

--- createZCCase.sh
+++ createZCCase.sh
@@ -28,6 +28,26 @@ cd Z-checker
 cp ../zc-patches/queryVarList $caseName
 cd ..
 
+##begin: Compressor sz_f
+echo Create a new case for sz_f
+cd ./SZ
+sz_caseName=${caseName}_fast
+mkdir -p $sz_caseName
+cd $sz_caseName
+cp $rootDir/zc-patches/test_CompDecomp.sh .
+cp $rootDir/zc-patches/zc-ratedistortion.sh .
+ln -s $rootDir/errBounds.cfg errBounds.cfg
+ln -s /home/sdi/Development/z-checker-installer/z-checker-installer/manageCompressor-sz-f.cfg manageCompressor.cfg
+ln -s $rootDir/manageCompressor manageCompressor
+./manageCompressor -z sz_f -c ./manageCompressor.cfg
+cd ./SZ/$sz_caseName
+ln -s ./SZ/example/./testfloat_CompDecomp ./testfloat_CompDecomp
+cd -
+cp $rootDir/Z-checker/examples/zc.config .
+cp SZ/example/sz.config ./SZ/$sz_caseName
+patch -p0 < $rootDir/zc-patches/zc-probe.config.patch
+cp $rootDir/zc-patches/queryVarList .
+##end: Compressor sz_f
 
 ##begin: Compressor sz_d
 echo "Create a new case (default mode) for SZ"
@@ -64,28 +84,9 @@ fi
 cp ../zfp-patches/*.sh $zfp_caseName
 cp utils/zc.config $zfp_caseName
 cp $rootDir/zc-patches/queryVarList $zfp_caseName
+cd ..
 ##end: Compressor zfp
 
-##begin: Compressor sz_f
-echo Create a new case for sz_f
-cd ./SZ
-sz_caseName=${caseName}_fast
-mkdir -p $sz_caseName
-cd $sz_caseName
-cp $rootDir/zc-patches/test_CompDecomp.sh .
-cp $rootDir/zc-patches/zc-ratedistortion.sh .
-ln -s $rootDir/errBounds.cfg errBounds.cfg
-ln -s /home/sdi/Development/z-checker-installer/z-checker-installer/manageCompressor-sz-f.cfg manageCompressor.cfg
-ln -s $rootDir/manageCompressor manageCompressor
-./manageCompressor -z sz_f -c ./manageCompressor.cfg
-cd ./SZ/$sz_caseName
-ln -s ./SZ/example/./testfloat_CompDecomp ./testfloat_CompDecomp
-cd -
-cp $rootDir/Z-checker/examples/zc.config .
-cp SZ/example/sz.config ./SZ/$sz_caseName
-patch -p0 < $rootDir/zc-patches/zc-probe.config.patch
-cp $rootDir/zc-patches/queryVarList .
-##end: Compressor sz_f
 ##New compressor to be added here
 
 cd $rootDir/zc-patches
