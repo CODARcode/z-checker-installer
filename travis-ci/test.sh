3a58d9e5aebbadf4b9fd84d1c466dbdffec2051e
Sheng Di <sdi1@anl.gov>
2018-12-05 14:02:47 -0600

    add pdfinfo

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -6,8 +6,6 @@ export PATH=$rootDir:.:$PATH
 
 cd $rootDir
 
-echo ------------------------------- CESM-ATM-Tylor --------------------------------
-app=CESM-ATM-Tylor
 compileMode='update'
 if [ ! -d SZ ]
 then
@@ -23,11 +21,43 @@ else
 	./z-checker-update.sh
 fi
 
+
+echo ------------------------------- CESM-ATM-Tylor --------------------------------
+app=CESM-ATM-Tylor
+
 echo ./createZCCase.sh $app
 ./createZCCase.sh $app
 
-echo ./runZCCase.sh -f REL $app $dataDir/CESM-ATM-Tylor f32 3600 1800
-./runZCCase.sh -f REL $app $dataDir/CESM-ATM-Tylor f32 3600 1800
+echo ./runZCCase.sh -f REL $app $dataDir/$app f32 3600 1800
+./runZCCase.sh -f REL $app $dataDir/$app f32 3600 1800
 
 echo ls -al Z-checker/$app/report
-ls -al Z-checker/$app/report
+ls -al Z-checker/$app/report/*.pdf
+pdfinfo Z-checker/$app/report/z-checker-report.pdf 
+
+echo ------------------------------- CESM-ATM-Tylor --------------------------------
+app=HACC
+
+echo ./createZCCase.sh $app
+./createZCCase.sh $app
+
+echo ./runZCCase.sh -f REL $app $dataDir/$app f32 131072
+./runZCCase.sh -f REL $app $dataDir/$app f32 131072 
+
+echo ls -al Z-checker/$app/report
+ls -al Z-checker/$app/report/*.pdf
+pdfinfo Z-checker/$app/report/z-checker-report.pdf 
+
+echo ------------------------------- CESM-ATM-Tylor --------------------------------
+app=Hurricane
+
+echo ./createZCCase.sh $app
+./createZCCase.sh $app
+
+echo ./runZCCase.sh -f REL $app $dataDir/$app f32 500 500 100
+./runZCCase.sh -f REL $app $dataDir/$app f32 500 500 100
+
+echo ls -al Z-checker/$app/report
+ls -al Z-checker/$app/report/*.pdf
+pdfinfo Z-checker/$app/report/z-checker-report.pdf 
+
