4605c473f46f255cdc5e00ed9d699ba5512c113c
Sheng Di <sdi1@anl.gov>
2019-05-07 00:29:18 -0500

    revise installHDF5Reader.sh

--- testHDF5/installHDF5Reader.sh
+++ testHDF5/installHDF5Reader.sh
@@ -4,6 +4,4 @@ cd ../Z-checker/HDF5Reader
 make -f Makefile.linux2 clean
 make -f Makefile.linux2
 cd -
-ln ../Z-checker/HDF5Reader/test/testHDF5_CompDecomp testHDF5_CompDecomp
-
-cp ../zc-patches/runZCCase_hdf5.sh .
+ln -s ../Z-checker/HDF5Reader/test/testHDF5_CompDecomp testHDF5_CompDecomp
