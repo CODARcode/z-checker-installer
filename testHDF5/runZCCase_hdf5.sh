1c816139b74964d88b8155e797f2d0964e23252d
Sheng Di <sdi1@anl.gov>
2019-05-07 10:32:42 -0500

    fix the workspace name for zfp (xxx_--> xxx)

--- testHDF5/runZCCase_hdf5.sh
+++ testHDF5/runZCCase_hdf5.sh
@@ -14,14 +14,14 @@ rootDir=`pwd`
 #copy results to sz_workspace
 sz_workspace=SZ/${workspace}_deft
 mkdir -p $sz_workspace/compressionResults
-echo mv compressionResults/SZ* $sz_workspace/compressionResults
-mv compressionResults/SZ* $sz_workspace/compressionResults
+echo mv compressionResults/sz_d* $sz_workspace/compressionResults
+mv compressionResults/sz_d* $sz_workspace/compressionResults
 
 #copy results to zfp_workspace
 zfp_workspace=zfp/${workspace}
 mkdir -p $zfp_workspace/compressionResults
-echo mv compressionResults/ZFP* $zfp_workspace/compressionResults
-mv compressionResults/ZFP* $zfp_workspace/compressionResults
+echo mv compressionResults/zfp* $zfp_workspace/compressionResults
+mv compressionResults/zfp* $zfp_workspace/compressionResults
 
 #create scripts for Z-checker workspace
 cd Z-checker
@@ -43,10 +43,44 @@ fi
 cd $workspace
 ./modifyZCConfig zc.config compressors "sz_d:../../SZ/${workspace}_deft zfp:../../zfp/${workspace}"
 
+############## as follows, it's comparison ##############
+
+sz_err_env="`cat ../../errBounds.cfg | grep -v "#" | grep comparisonCases`"
+
+echo "export $sz_err_env" > env.tmp
+source env.tmp
+rm env.tmp
+SZ_Err_Bounds="`echo $comparisonCases`"
+
+echo comparisonCases=$comparisonCases
+./modifyZCConfig zc.config comparisonCases "$comparisonCases"
+
+zc_err_env="`cat ../../errBounds.cfg | grep -v "#" | grep numOfErrorBoundCases`"
+
+echo "export $zc_err_env" > env.tmp
+source env.tmp
+rm env.tmp
+ZC_Err_Bounds="`echo $numOfErrorBoundCases`"
+
+echo numOfErrorBoundCasess=$numOfErrorBoundCases
+./modifyZCConfig zc.config numOfErrorBoundCases "$numOfErrorBoundCases"
+
+if [[ $errBoundMode == "PW_REL" ]]; then
+        echo ./generateReport.sh ${testcase} with PW_REL
+        ./generateReport.sh "${testcase} with PW_REL"
+else
+        echo ./generateReport.sh ${testcase}
+        ./generateReport.sh "${testcase}"
+fi
+
 #remove useless files
 cd $rootDir
 rm -rf compressionResults
 rm -rf dataProperties
 
-echo The dataProperties and compressionResults have been moved to the workspace $workspace
+if [ -f Z-checker/${workspace}/report/z-checker-report.pdf ];then
+	ln -f -s Z-checker/${workspace}/report/z-checker-report.pdf z-checker-report.pdf
+	echo The z-checker report has been generated (z-checker-report.pdf)
+fi
+
 echo done
