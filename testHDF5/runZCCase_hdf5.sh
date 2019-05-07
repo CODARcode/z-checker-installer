6d7964104cbd30e29e83d33f999ab0b74f439b72
Sheng Di <sdi1@anl.gov>
2019-05-07 00:35:41 -0500

    revise runZCCase_hdf5.sh

--- testHDF5/runZCCase_hdf5.sh
+++ testHDF5/runZCCase_hdf5.sh
@@ -10,13 +10,19 @@ fi
 workspace=$1
 
 mkdir -p ../SZ/${workspace}_deft/compressionResults
+echo mv compressionResults/SZ* ../SZ/${workspace}_deft/compressionResults
 mv compressionResults/SZ* ../SZ/${workspace}_deft/compressionResults
 
 mkdir -p ../zfp/${workspace}/compressionResults
+echo mv compressionResults/ZFP* ../zfp/${workspace}/compressionResults
 mv compressionResults/ZFP* ../zfp/${workspace}/compressionResults
 
 mkdir -p ../Z-checker/${workspace}/dataProperties
+echo mv dataProperties/* ../Z-checker/${workspace}/dataProperties
 mv dataProperties/* ../Z-checker/${workspace}/dataProperties
 
 rm -rf compressionResults
 rm -rf dataProperties
+
+echo The dataProperties and compressionResults have been moved to the workspace $workspace
+echo done
