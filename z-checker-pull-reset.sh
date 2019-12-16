617450de366b29b9b4e30e401b077cb27e559e8d
Sheng Di <sdi1@anl.gov>
2019-12-16 11:30:13 -0600

    added *_libpressio.c in z-checker-pull-reset.sh

--- z-checker-pull-reset.sh
+++ z-checker-pull-reset.sh
@@ -216,7 +216,9 @@ cd ..
 
 cd example
 cp ../../sz-patches/testfloat_CompDecomp.c .
+cp ../../sz-patches/testfloat_CompDecomp_libpressio.c .
 cp ../../sz-patches/testdouble_CompDecomp.c .
+cp ../../sz-patches/testdouble_CompDecomp_libpressio.c .
 cp ../../sz-patches/sz-zc-vis.c .
 cp ../../sz-patches/Makefile.bk .
 make clean -f Makefile.bk
