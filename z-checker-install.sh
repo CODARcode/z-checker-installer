39c94c71fd9a46e315e681909f0f63b47845858f
disheng222 <disheng222@gmail.com>
2020-12-07 22:06:32 -0600

    fix the compilation issue about manageCompressor.c in z-checker-installer

--- z-checker-install.sh
+++ z-checker-install.sh
@@ -188,7 +188,7 @@ export PATH=$rootDir/Z-checker/zc-install/bin:$PATH
 cp ../zc-patches/generateReport.sh ./examples/
 
 cd $rootDir/zc-patches
-gcc -g -O0 -o manageCompressor manageCompressor.c -I../Z-checker/zc-install/include -L../Z-checker/zc-install/lib -lzc -lm -Wl,-rpath $rootDir/Z-checker/zc-install/lib
+gcc -g -O3 -o manageCompressor manageCompressor.c -fPIC -I../Z-checker/zc-install/include -L../Z-checker/zc-install/lib -lzc -lm -Wl,-rpath $rootDir/Z-checker/zc-install/lib -I$(LibpressioOptPrefixDir)/include/libpressio -L$(LibpressioOptPrefixDir)/lib64 -llibpressio -Wl,-rpath,"$(LibpressioOptPrefixDir)/lib64"
 mv manageCompressor ..
 
 #---------- download ZFP and set the configuration -----------
